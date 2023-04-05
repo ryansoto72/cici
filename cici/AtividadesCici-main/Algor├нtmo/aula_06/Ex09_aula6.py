9043bbb97c8fb34bc55ba04ada52c836e467cfdb
srfrogui <omarrenardo33801@gmail.com>
2023-04-05 10:19:56 -0300

    9 e 10 resolvido com if(valor invalido)

--- cici/AtividadesCici-main/Algor├нtmo/aula_06/Ex09_aula6.py
+++ cici/AtividadesCici-main/Algor├нtmo/aula_06/Ex09_aula6.py
@@ -1,16 +1,14 @@
 peso = int(input("Digite seu peso (em kg): "))
 altura = int(input("Digite sua altura (em cm): "))
 
-altura = altura / 100
-massa = peso / (altura*2)
+if peso > 0 or altura > 0:
+      altura = altura / 100
+      massa = peso / (altura*2)
 
-if massa < 26: 
-    grau = "Normal"
-
-elif 26 <= massa < 30: 
-      grau = "Obeso"
-  
-else: 
-      grau = "Obeso Morbido"
-  
-print(f"Com o peso de {peso} kilos e a altura de {altura} kg, o seu índice de massa corporal é {massa:.2f}, o que indica obesidade {grau}.")
+      if massa < 26: grau = "Normal"
+      elif 26 <= massa < 30: grau = "Obeso"
+      else: grau = "Obeso Morbido"
+      
+      print(f"Com o peso de {peso} quilos e a altura de {altura} metros, o seu índice de massa corporal é {massa:.2f}, o que indica obesidade {grau}.")
+else:
+      print('Peso invalido')
